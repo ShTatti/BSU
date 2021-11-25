09e03208a3652898e72ca39d96339a44d37a8a00
ShTatti <tanushash04@gmail.com>
2021-11-25 11:20:50 +0300

    Update func_test.cpp

--- laba6/func_test.cpp
+++ laba6/func_test.cpp
@@ -1,7 +1,13 @@
 #include "gtest/gtest.h"
 #include "func.h"
 
-TEST(constructor, test1){
+TEST(GetNumeratorOfFraction, test1) {
+    ProperFraction fraction(3,4);
+    int a = fraction.GetNumerator(fraction);
+    ASSERT_EQ(a,3);
+}
+
+TEST(СonstructorOfFrection, test1) {
     ProperFraction fraction(1,2);
     int a = fraction.GetNumerator(fraction);
     int b = fraction.GetDenominator(fraction);
@@ -9,7 +15,7 @@ TEST(constructor, test1){
     ASSERT_EQ(b,2);
 }
 
-TEST(reducing, test2) {
+TEST(ReducingOfFraction, test1) {
     ProperFraction fraction(2, 4);
     int a = fraction.GetNumerator(fraction);
     int b = fraction.GetDenominator(fraction);
@@ -17,18 +23,54 @@ TEST(reducing, test2) {
     ASSERT_EQ(b,2);
 }
 
-TEST(sum, test3) {
-    ProperFraction a(1, 2);
-    ProperFraction b(1, 3);
-    ProperFraction c = a + b;
-    ASSERT_EQ(c.GetNumerator(c), 5);
-    ASSERT_EQ(c.GetDenominator(c), 6);
+TEST(MinusInDenominator, test1) {
+    ProperFraction fraction(4, -9);
+    ASSERT_EQ(fraction.GetNumerator(fraction), 4);
+    ASSERT_EQ(fraction.GetDenominator(fraction), -9);
 }
 
-TEST(minus, test4) {
-    ProperFraction a(8, 5);
-    ProperFraction b(4, 7);
-    ProperFraction c = a - b;
-    ASSERT_EQ(c.GetNumerator(c), 36);
-    ASSERT_EQ(c.GetDenominator(c), 35);
+TEST(MinusInDenominatorAndNumerator, test1) {
+    ProperFraction fraction(-4, -9);
+    ASSERT_EQ(fraction.GetNumerator(fraction), -4);
+    ASSERT_EQ(fraction.GetDenominator(fraction), -9);
+}
+
+TEST(NumeratorWithNull, test1){
+    ProperFraction fraction(0,4);
+    int a = fraction.GetNumerator(fraction);
+    int b = fraction.GetDenominator(fraction);
+    ASSERT_EQ(a,0);
+    ASSERT_EQ(b,4);
+}
+
+TEST(FractionMultiplication, test1) {
+    ProperFraction fraction1(1, 4);
+    ProperFraction fraction2(1, 3);
+    fraction1.FractionMultiplication(fraction2);
+    ASSERT_EQ(fraction1.GetNumerator(fraction1), 1);
+    ASSERT_EQ(fraction1.GetDenominator(fraction1), 12);
+}
+
+TEST(FractionSum, test1) {
+    ProperFraction fraction1(1, 4);
+    ProperFraction fraction2(1, 3);
+    fraction1.FractionSum(fraction2);
+    ASSERT_EQ(fraction1.GetNumerator(fraction1), 7);
+    ASSERT_EQ(fraction1.GetDenominator(fraction1), 12);
+}
+
+TEST(FractionDivisionThesame, test1){
+    ProperFraction fraction1(1,4);
+    ProperFraction fraction2(1,4);
+    fraction1.FractionDivision(fraction2);
+    ASSERT_EQ(fraction1.GetNumerator(fraction1), 4);
+    ASSERT_EQ(fraction1.GetDenominator(fraction1), 4);
+}
+
+TEST(FractionDivision, test1){
+    ProperFraction fraction1(1,4);
+    ProperFraction fraction2(3,7);
+    fraction1.FractionDivision(fraction2);
+    ASSERT_EQ(fraction1.GetNumerator(fraction1), 7);
+    ASSERT_EQ(fraction1.GetDenominator(fraction1), 12);
 }
